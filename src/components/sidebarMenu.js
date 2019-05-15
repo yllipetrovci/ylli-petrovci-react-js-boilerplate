53f4f2bb4d286288c1ddce0f77a80bda607996bc
Ylli <yllipetrovci@gmail.com>
2019-05-15 21:57:27 +0200

    add sidebar component

---
+++ src/components/sidebarMenu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/sidebar.css';
+
+const SideBarMenu = (props) => {
+    return (
+        <div className="sidebar-menu-component">
+            <h2>SideBarMenu</h2>
+        </div>
+    )
+}
+
+export default SideBarMenu;
