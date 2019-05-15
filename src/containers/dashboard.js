593935c2c74663a72fd1c0b7a76bdefeb447cd85
Ylli <yllipetrovci@gmail.com>
2019-05-15 21:57:38 +0200

    add dashboard container

---
+++ src/containers/dashboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../styles/dashboard.css';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div className="dashboard-view">
+                <h1>Dashboard</h1>
+            </div>
+        )
+    }
+}
+
+export default Dashboard;
