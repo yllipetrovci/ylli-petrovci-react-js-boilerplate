da76a2345d2f03732c7c022a6955136abc7ae898
Ylli <yllipetrovci@gmail.com>
2019-05-15 22:23:52 +0200

    add config for setuping routes

---
+++ src/setupRoutes.js
@@ -0,0 +1,12 @@
+/*
+#Default Routes
+*/
+const routes = [
+    { name: 'Dashboard', url: '/', icon: '' },
+    { name: 'Page 1', url: '/page1', icon: '' },
+    { name: 'Page 2', url: '/page2', icon: '' },
+];
+/*
+#Allow other components consume routes array
+*/
+export default routes;
