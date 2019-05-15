d00fdc9e5c21e77508402528437e85a0b35f453e
Ylli <yllipetrovci@gmail.com>
2019-05-15 22:24:18 +0200

    enhance sidebardMenu logo and show routes

--- src/components/sidebarMenu.js
+++ src/components/sidebarMenu.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import '../styles/sidebar.css';
+import setupRoutes from '../setupRoutes';
 
 const SideBarMenu = (props) => {
     return (
         <div className="sidebar-menu-component">
-            <h2>SideBarMenu</h2>
+            <div className="row">
+                <div className="col-12">
+                    <h3 className="text-center">Logo</h3>
+                </div>
+                <hr />
+                <div className="col-12">
+                    <ul>
+                        {/* #display routes */}
+                        {setupRoutes.map((route, index) => (
+                            <li>{route.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
