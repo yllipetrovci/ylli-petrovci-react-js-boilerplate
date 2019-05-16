80eea7971d7981211b7cb3dfc7825d00e336e05e
Ylli <yllipetrovci@gmail.com>
2019-05-16 22:26:22 +0200

    show user info and img

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../styles/header.css';
+import user_img from '../assets/user-avatar.png';
 
 const Header = (props) => {
     return (
-        <div className="header-component">
-            <header className="text-center"></header>
-        </div>
+        <header className="header-component">
+            <div className="row">
+                <div className="col-5">
+                    <button type="button" className="btn btn-success">Collapse</button>
+                </div>
+                <div className="offset-3 col-2 text-right">
+                    <img src={user_img} alt="user-avatar" className="avatar-img rounded-circle" />
+                </div>
+                <div className="pl-0 col-2">
+                    <p className="username-paragraph">userame</p>
+                </div>
+            </div>
+        </header>
     )
 }
 
