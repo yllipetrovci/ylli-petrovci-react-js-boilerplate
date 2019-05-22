6d0de7ad4c36a64dd0aeb16513f89cef7ec49911
Ylli <yllipetrovci@gmail.com>
2019-05-22 23:53:56 +0200

    Implement recent activites component

---
+++ src/components/recent-activites.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const RecentActivities = (props) => {
+
+    return (
+        <div className="recent-activities-component">
+            <ul>
+                <li>
+                    Test 1
+                </li>
+                <li>
+                    Test 2
+                </li>
+                <li>
+                    Test 3
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default RecentActivities;
