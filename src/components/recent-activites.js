c76dbb29a5ae64755a1cba0d5215d62a0ed8d448
Ylli <yllipetrovci@gmail.com>
2019-05-23 22:52:53 +0200

    add recent activites style

--- src/components/recent-activites.js
+++ src/components/recent-activites.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/recent-activities.scss';
 
 const RecentActivities = (props) => {
 
--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/dashboard.scss';
+import RecentActivities from '../components/recent-activites';
 
 class Dashboard extends Component {
     render() {
         return (
             <div className="dashboard-view">
                 <h1>Dashboard</h1>
+                <RecentActivities/>
             </div>
         )
     }
