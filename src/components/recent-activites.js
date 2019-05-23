4e70a3ff479d68375b9380069597125dbb706e54
Ylli <yllipetrovci@gmail.com>
2019-05-23 23:50:29 +0200

    fix color for recent activites

--- src/components/recent-activites.js
+++ src/components/recent-activites.js
@@ -6,46 +6,70 @@ const RecentActivities = (props) => {
 
     return (
         <div className="recent-activities-component">
-            <ul>
-                <li>
-                    <div className="row">
-                        <div className="col-12 pb-4 pt-4">
-                            <h4 className="text-left">RECENT ACTIVITY</h4> 
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div className="row">
-                        <div className="col-4 text-right pt-2">
-                            <img src={user_img} alt="user-avatar" className="rounded-circle" />
-                        </div>
-                        <div className="col-8 text-left pt-2 pl-0">
-                            <h5 className="mb-0">Angelia Joile</h5>
-                            <p>Liked your post</p>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div className="row">
-                        <div className="col-5 text-right pt-2">
-                            <img src={user_img} alt="user-avatar" className="rounded-circle" />
-                        </div>
-                        <div className="col-7 text-center pt-3">
-                            <h5>Test 2</h5>
-                        </div>
-                    </div>
-                </li>
-                <li>
-                    <div className="row">
-                        <div className="col-5 text-right pt-2">
-                            <img src={user_img} alt="user-avatar" className="rounded-circle" />
-                        </div>
-                        <div className="col-7 text-center pt-3">
-                            <h5>Test 3</h5>
-                        </div>
-                    </div>
-                </li>
-            </ul>
+            <div className="row">
+                <div className="col-12">
+                    <ul>
+                        <li>
+                            <div className="row">
+                                <div className="col-12 pt-5 pb-5">
+                                    <h4 className="text-center">RECENT ACTIVITY</h4>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-4 text-right pt-2">
+                                    <img src={user_img} alt="user-avatar" className="rounded-circle" />
+                                </div>
+                                <div className="col-8 text-left pt-2 pl-0">
+                                    <div className="row">
+                                        <div className="col-12">
+                                            <h5 className="mb-0">Angelia Joile</h5>
+                                        </div>
+                                        <div className="col-12">
+                                            <p>Liked your post</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-4 text-right pt-2">
+                                    <img src={user_img} alt="user-avatar" className="rounded-circle" />
+                                </div>
+                                <div className="col-8 text-left pt-2 pl-0">
+                                    <div className="row">
+                                        <div className="col-12">
+                                            <h5 className="mb-0">Angelia Joile</h5>
+                                        </div>
+                                        <div className="col-12">
+                                            <p>Liked your post</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div className="row">
+                                <div className="col-4 text-right pt-2">
+                                    <img src={user_img} alt="user-avatar" className="rounded-circle" />
+                                </div>
+                                <div className="col-8 text-left pt-2 pl-0">
+                                    <div className="row">
+                                        <div className="col-12">
+                                            <h5 className="mb-0">Angelia Joile</h5>
+                                        </div>
+                                        <div className="col-12">
+                                            <p>Liked your post</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -6,12 +6,16 @@ class Dashboard extends Component {
     render() {
         return (
             <div className="dashboard-view">
-                <h1>Dashboard</h1>
-                <div className="row ">
-                    <div className="offset-9 col-3 text-right">
-                        <RecentActivities />
+                {/* <div className="container"> */}
+                    <div className="row">
+                        <div className="col-9">
+                            <h1>Dashboard</h1>
+                        </div>
+                        <div className="col-3 text-right">
+                            <RecentActivities />
+                        </div>
                     </div>
-                </div>
+                {/* </div> */}
             </div>
         )
     }
