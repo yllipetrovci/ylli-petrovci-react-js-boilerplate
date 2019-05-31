4864bff0c0f43136e4ddec3f03c132ea8757e7ee
yllipetrovci <yllipetrovci@gmail.com>
2019-06-01 00:46:36 +0200

    add input component, add styling file for input too

---
+++ src/components/input.js
@@ -0,0 +1,10 @@
+import React  from 'react';
+import '../styles/components/input.scss';
+
+const Input = (props)=>(
+    <div className="input-component">
+            <input type="text" placeholder="Write here"/>
+    </div>
+);
+
+export default Input;
