7c78acea79db81cfc3c7d7ee8ee0f0afb773a1fc
yllipetrovci <yllipetrovci@gmail.com>
2019-06-01 00:31:47 +0200

    remove computed match warning

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';// Redirect
-import Main from './containers/dashboard';
+import Dashboard from './containers/dashboard';
 import SidebardMenu from './components/sidebarMenu';
 import Header from './components/header';
 
@@ -8,22 +8,21 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <div className="row row ml-0 mr-0">
-
-            <div className="col-12 col-md-2 pr-0 pl-0">
-              <SidebardMenu />
-            </div>
-            <div className="col-12 col-md-10 pl-md-0 pr-0">
-              <div className='pl-0 col-12 pr-0'>
-                <div className="container">
-                  <Header name="Name" />
-                </div>
-              </div>
-              <Route exact path="/" component={Main} />
-            </div>
+        <div className="row row ml-0 mr-0">
+          <div className="col-12 col-md-2 pr-0 pl-0">
+            <SidebardMenu />
           </div>
-        </Switch>
+          <div className="col-12 col-md-10 pl-md-0 pr-0">
+            <div className='pl-0 col-12 pr-0'>
+              <div className="container">
+                <Header name="Name" />
+              </div>
+            </div>
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
       </BrowserRouter>
     );
   }
