50dc55960543f476b7fc69c258a7374daba8fb53
yllipetrovci <yllipetrovci@gmail.com>
2019-06-01 02:02:22 +0200

    implement design for cards

--- src/components/card.js
+++ src/components/card.js
@@ -2,17 +2,18 @@ import React from 'react';
 import '../styles/components/card.scss';
 
 const Card = (props) => {
+
     return (
-        <div className="card-component">
-            <div className="row">
-                <div className="col-12">
+        <div className="card-component rounded">
+            <div className="row text-center">
+                <div className="col-12 pb-2 pt-2">
                     <img src={props.icon} />
                 </div>
                 <div className="col-12">
-                    {props.title}
+                    <p className="third-color">{props.title}</p>
                 </div>
-                <div className="col-12">
-                    {props.value}
+                <div className="col-12 pb-1">
+                    <h4 className={props.color ? props.color : 'first-color'}>{props.value}</h4>
                 </div>
             </div>
         </div>
