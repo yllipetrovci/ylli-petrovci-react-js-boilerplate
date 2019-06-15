61bca3bb91b380a69f380c1431b731a0746b60f6
yllipetrovci <yllipetrovci@gmail.com>
2019-06-15 12:09:30 +0200

    use activites in recent activites component

--- src/components/recent-activites.js
+++ src/components/recent-activites.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import '../styles/recent-activities.scss';
-import user_img from '../assets/user-avatar.png';
 import angelinaJoliImg from '../assets/users/angelina-joli.png';
 import bradleyCooperImg from '../assets/users/bradley-cooper.png';
-import hughLaurieImg from '../assets/users/hugh-laurie.png';
 import jeniferAnistonImg from '../assets/users/jenifer-aniston.png';
+import hughLaurie from '../assets/users/hugh-laurie.png';
+import Activity from './activity';
 
 const RecentActivities = (props) => {
 
@@ -20,74 +20,10 @@ const RecentActivities = (props) => {
                                 </div>
                             </div>
                         </li>
-                        <li>
-                            <div className="row">
-                                <div className="col-4 text-right pt-2">
-                                    <img src={angelinaJoliImg} alt="user-avatar" className="rounded-circle" />
-                                </div>
-                                <div className="col-8 text-left pt-2 pl-0">
-                                    <div className="row">
-                                        <div className="col-12">
-                                            <h5 className="mb-0">Angelia Joile</h5>
-                                        </div>
-                                        <div className="col-12">
-                                            <p>Liked your post</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="row">
-                                <div className="col-4 text-right pt-2">
-                                    <img src={bradleyCooperImg} alt="user-avatar" className="rounded-circle" />
-                                </div>
-                                <div className="col-8 text-left pt-2 pl-0">
-                                    <div className="row">
-                                        <div className="col-12">
-                                            <h5 className="mb-0">Bradley Cooper</h5>
-                                        </div>
-                                        <div className="col-12">
-                                            <p>Commented your profile</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="row">
-                                <div className="col-4 text-right pt-2">
-                                    <img src={jeniferAnistonImg} alt="user-avatar" className="rounded-circle" />
-                                </div>
-                                <div className="col-8 text-left pt-2 pl-0">
-                                    <div className="row">
-                                        <div className="col-12">
-                                            <h5 className="mb-0">Jenifer Aniston</h5>
-                                        </div>
-                                        <div className="col-12">
-                                            <p>Started following you</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <div className="row">
-                                <div className="col-4 text-right pt-2">
-                                    <img src={jeniferAnistonImg} alt="user-avatar" className="rounded-circle" />
-                                </div>
-                                <div className="col-8 text-left pt-2 pl-0">
-                                    <div className="row">
-                                        <div className="col-12">
-                                            <h5 className="mb-0">Hugh Laurie</h5>
-                                        </div>
-                                        <div className="col-12">
-                                            <p>Commented your profile</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </li>
+                        <Activity title={"Angelina Joli"} action={"liked your post"} img={angelinaJoliImg} />
+                        <Activity title={'Bradley Cooper'} action={"Commented your profile"} img={bradleyCooperImg} />
+                        <Activity title={'Jenifer Aniston'} action={"Started following you"} img={jeniferAnistonImg} />
+                        <Activity title={'Hugh Laurie'} action={"Commented your profile"} img={hughLaurie} />
                         <li className="text-center">
                             <button type="button view-more">VIEW MORE</button>
                         </li>
